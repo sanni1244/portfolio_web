5dc466ac8ef34d79c61eaaaf588063d2eea959f3
Sanni Opeyemi <opeyemi.sanni180591052@st.lasu.edu.ng>
2024-03-25 13:11:19 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        DarkGray: '#292929',
+        LightGray: '#F4F4F4',
+        VibrantOrange: '#FF5733',
+        // ...other custom colors
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
